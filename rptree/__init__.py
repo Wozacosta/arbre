4a3822f2dcc3a1f659d837fc8e8089c4d70a4a20
Wozacosta <rabah.m.samy@gmail.com>
2021-05-09 11:55:39 +0200

    tree generator base

---
+++ rptree/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level packae for RP Tree."""
+
+__version__ = "0.1.0"
---
+++ rptree/rptree.py
@@ -0,0 +1,52 @@
+"""This module provides RP Tree main module."""
+
+import os
+import pathlib
+
+PIPE = "│"
+ELBOW = "└──"
+TEE = "├──"
+PIPE_PREFIX = "│   "
+SPACE_PREFIX = "    "
+
+class _TreeGenerator:
+    def __init__(self, root_dir):
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def build_tree(self):
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
+
+    def _tree_body(self, directory, prefix=""):
+        entries = directory.iterdir()
+        entries = sorted(entries, key=lambda entry: entry.is_file())
+        entries_count = len(entries)
+        for index, entry in enumerate(entries):
+            connector = ELBOW if index == entries_count - 1 else TEE
+            if entry.is_dir():
+                self._add_directory(
+                    entry, index, entries_count, prefix, connector
+                )
+            else:
+                self._add_file(entry, prefix, connector)
+
+    def _add_directory(self, entry, index, entries_count, prefix, connector):
+        ...
+
+    def _add_file(self, entry, prefix, connector):
+        ...
+
+class DirectoryTree:
+    def __init__(self, root_dir):
+        self._generator = _TreeGenerator(root_dir)
+
+    def generate(self):
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
